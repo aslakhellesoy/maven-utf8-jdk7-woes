83eaad9dd2e1ccb83a29cb9db56ed2041e19cde2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2012-10-04 20:40:40 +0100

    More weird

--- Ls.java
+++ Ls.java
@@ -3,10 +3,18 @@ import java.io.IOException;
 
 // See http://jonisalonen.com/2012/java-and-file-names-with-invalid-characters/
 class Ls {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         File d = new File("src/main/java/com/aslakhellesoy");
         for (File f : d.listFiles()) {
-            System.out.printf("%s: %b\n", f.getName(), f.exists());
+            if(args.length > 0 && "hex".equals(args[0])) {
+                System.out.println(toHex(f.getName()));
+            } else {
+                System.out.printf("%s: %b\n", f.getName(), f.exists());
+            }
         }
+    }
+
+    private static String toHex(String arg) throws Exception {
+        return String.format("%040x", new java.math.BigInteger(arg.getBytes("UTF-8")));
     }
 }
